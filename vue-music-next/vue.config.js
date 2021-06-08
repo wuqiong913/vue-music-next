aeb6c4d4de2284e9c50af66123ff8e1089fbcb43
wuqiong913 <892928242@qq.com>
2021-06-08 14:11:44 +0800

    basic

---
+++ vue-music-next/vue.config.js
@@ -0,0 +1,16 @@
+
+
+module.exports = {
+  css: {
+    loaderOptions: {
+      sass: {
+        // 全局引入变量和 mixin
+        additionalData: `
+          @import "@/assets/scss/variable.scss";
+          @import "@/assets/scss/mixin.scss";
+        `
+      }
+    }
+  },
+
+}
